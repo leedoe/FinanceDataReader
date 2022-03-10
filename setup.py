afa0344cbea187647ee00b9757d364cc8da963eb
DOJUN <leedoe0709@gmail.com>
2022-03-10 15:04:04 +0900

    ENH: setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='finance_data_reader',
+    version='1.0.0',
+    url='https://github.com/leedoe/FinanceDataReader.git',
+    install_requires=[
+        'pandas', 'requests', 'beautifulsoup4'
+    ]
+)
