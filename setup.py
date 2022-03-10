e89ad5bdad265264cada051477ebe69368a062d3
DOJUN <leedoe0709@gmail.com>
2022-03-10 15:05:15 +0900

    ENH: 이름변경

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(
-    name='finance_data_reader',
+    name='financedatareader',
     version='1.0.0',
     url='https://github.com/leedoe/FinanceDataReader.git',
     install_requires=[
